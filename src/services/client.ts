5d494a2ff928138bdc2e6d41e29885ba611ec45b
Nguy Nhu An <62284926+localhostvn@users.noreply.github.com>
2022-09-30 00:52:59 +0700

    Update client.ts

--- src/services/client.ts
+++ src/services/client.ts
@@ -9,23 +9,4 @@ const client = axios.create({
     deviceid : "",
   },
 });
-const fetchApi = (data) => {
-  return axios.get(
-    `${baseUrl}${data.path}`, 
-    { 
-      headers,
-      params : data.params
-    }
-  )
-}
-export const postFetchApi = (data) => {
-  return axios.post( 
-    `${baseUrl}${data.path}`,
-    data.params,
-    {
-      headers,
-    }
-  )
-}
-export default fetchApi;
 export default client;
